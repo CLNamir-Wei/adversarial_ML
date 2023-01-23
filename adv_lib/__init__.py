65aee576341176737e971c0c6dd20363858767d6
namir <10326weichinglin@gmail.com>
2023-01-23 15:15:54 +0800

    add transfer attack

--- adv_lib/__init__.py
+++ adv_lib/__init__.py
@@ -19,13 +19,15 @@ model_weigts = {
 
 
 def load_pytorch_pre_trained_model(model_name:str, n_class:int) -> Callable:
+
     model = getattr(import_module('torchvision.models.detection'), model_name)
     weights = getattr(import_module(
         'torchvision.models.detection'), model_weigts[model_name])
     return model(weights=weights.DEFAULT, num_classes=n_class)
 
 
-def torch_predict_method(torch_model: Callable, img_data_loader: DataLoader) ->List:
+def torch_predict_method(torch_model: Callable, img_data_loader: DataLoader) -> List:
+    
     torch_model.eval()
     outputs = []
     for data_pair in img_data_loader:
--- run_gradient_adv.py
+++ run_gradient_adv.py
@@ -7,9 +7,12 @@ from adv_lib import load_pytorch_pre_trained_model, ModifyPyTorchObjectDetector
 
 
 def get_parser():
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str,
-                        help='the name of a pre-trained pytorch model')
+    parser.add_argument('--source_model', type=str,
+                        help='the name of a pre-trained pytorch model for the white box attack')
+    parser.add_argument('--victim_model', type=str,
+                        help='the name of a pre-trained pytorch model for the transfer attack', required=False)
     parser.add_argument('--img_type', type=str, default='jpg',
                         help='optional, the image file type; default is jpg', required=False)
     parser.add_argument('--raw_image_root', type=str,
@@ -34,16 +37,21 @@ if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
 
-    model_name = args.model_name
-    target_model = load_pytorch_pre_trained_model(model_name, 91)
-    target_model = ModifyPyTorchObjectDetector(
-        model=target_model, clip_values=(0, 255))
+    source_model = load_pytorch_pre_trained_model(args.source_model, 91)
+    source_model = ModifyPyTorchObjectDetector(
+        model=source_model, clip_values=(0, 255))
+
+    if args.victim_model is None:
+        victim_model = source_model
+    else:
+        victim_model = load_pytorch_pre_trained_model(args.victim_model, 91)
+        victim_model = ModifyPyTorchObjectDetector(model=victim_model, clip_values=(0,255))
 
     raw_img_names = [p for p in os.listdir(args.raw_image_root) if re.search(
         "%s$" % args.img_type, p) is not None]
     adv_img_names = ['adv_'+name for name in raw_img_names]
 
-    pgd = eva.ProjectedGradientDescent(estimator=target_model,
+    pgd = eva.ProjectedGradientDescent(estimator=source_model,
                                        eps=args.atk_eps, eps_step=args.atk_eps_step, max_iter=args.atk_max_iter)
     adv_generator = AdvImgCreator(args.raw_image_root, args.adv_image_root)
 
@@ -54,11 +62,11 @@ if __name__ == "__main__":
         adv_generator.create_img(pgd, raw, adv)
         if args.display_raw_pred_root is not None:
             raw_pred_plotor = SavePlotWithPrediction(
-                target_model, pred, args.display_raw_pred_root)
+                victim_model, pred, args.display_raw_pred_root)
             raw_pred_plotor.display_model_prediction(
                 adv_generator.img_arr, raw, box_fornt_size)
         if args.display_adv_pred_root is not None:
             adv_pred_plotor = SavePlotWithPrediction(
-                target_model, pred, args.display_adv_pred_root)
+                victim_model, pred, args.display_adv_pred_root)
             adv_pred_plotor.display_model_prediction(
                 adv_generator.atk_img_arr, adv, box_fornt_size)
